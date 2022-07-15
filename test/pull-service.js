e1cc0871685cdb8714d584f39f72617b08ebdf9c
RealLau <921363139@qq.com>
2022-07-15 16:44:43 +0800

    debug slack notification

--- test/pull-service.js
+++ test/pull-service.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 describe('opensea', function () {
     describe('collections', function () {
         it('all', function () {
-            console.log(`assert:`)
+            console.log(`111`)
             assert.equal([1, 2, 3].indexOf(4), -1);
         });
     });
