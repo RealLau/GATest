654304076aa129ce91227c3a0b963f4ff63017e5
RealLau <921363139@qq.com>
2022-07-15 11:12:11 +0800

    add sample tests and modify actions to do a test

---
+++ test/pull-service.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+describe('opensea', function () {
+    describe('collections', function () {
+        it('all', function () {
+            assert.equal([1, 2, 3].indexOf(4), -1);
+        });
+    });
+});
